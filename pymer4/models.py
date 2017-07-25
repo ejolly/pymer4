4e18ab574859a5b1da286b516d7815ecf7dab6cd
ejolly <eshin.jolly@gmail.com>
2017-07-24 23:35:34 -0400

    Handle cases where lmerTest chokes and doesn't return p-values.

--- pymer4/models.py
+++ pymer4/models.py
@@ -5,6 +5,8 @@ import rpy2
 from copy import copy
 import pandas as pd
 import numpy as np
+import warnings
+warnings.simplefilter('always',UserWarning)
 pandas2ri.activate()
 
 class Lmer(object):
@@ -88,12 +90,41 @@ class Lmer(object):
 
         return r_df
 
-    def fit(self,conf_int='profile',factors=None):
+    def anova(self):
+
+        raise NotImplementedError(" ANOVA tables coming soon!")
+
+        #TODO Make sure factors are set with orthogonal contrasts first
+        """Compute a Type III ANOVA table on a fitted model."""
+
+        assert self.fitted == True, "Model hasn't been fit! Call fit() method before computing ANOVA table."
+
+        #See rpy2 for building contrasts or loop and construct rstring
+        rstring = """
+            function(model){
+            df<- anova(model)
+            df
+            }
+        """
+        anova = robjects.r(rstring)
+        self.anova = pandas2ri.ri2py(anova)
+        if self.anova.shape[1] == 6:
+                self.anova.columns = ['SS','MS','NumDF','DenomDF','F-stat','P-val']
+                self.anova['Sig'] = self.anova['P-val'].apply(lambda x: self._sig_stars(x))
+        elif self.anova.shape[1] == 4:
+            warnings.warn("P-value computation did not occur because lmerTest choked. Check model specification or data...")
+            self.anova.columns = ['DF','SS','MS','F-stat']
+        print("Analysis of variance Table of type III with Satterthwaite approximated degrees of freedom:\n")
+        return self.anova
+
+
+    def fit(self,conf_int='profile',factors=None,summary=True):
         """Main method for fitting model object. Will modify the model's data attribute to add columns for residuls and fits for convenience.
 
         Args:
             conf_int: (string) which method to compute confidence intervals; profile (default), Wald, or boot (parametric bootstrap)
             factors: (dict) dictionary of col names (keys) to treat as dummy-coded factors with levels specified by unique values (vals). First level is always reference, e.g. {'Col1':['A','B','C']}
+            summary: (bool) whether to print a model summary after fitting (default is True)
 
         Returns:
             coefs: (dataframe) dataframe of R style summary() table
@@ -102,6 +133,7 @@ class Lmer(object):
         """
         if factors:
             dat = self._make_factors(factors)
+            self.factors = factors
         else:
             dat = self.data
 
@@ -213,9 +245,15 @@ class Lmer(object):
             estimates_func = robjects.r(rstring)
             df = pandas2ri.ri2py(estimates_func(model))
 
-            df.columns = ['Estimate','SE','DF','T-stat','P-val','2.5_ci','97.5_ci']
+            if df.shape[1] == 7:
+                df.columns = ['Estimate','SE','DF','T-stat','P-val','2.5_ci','97.5_ci']
+                df = df[['Estimate','2.5_ci','97.5_ci','SE','DF','T-stat','P-val']]
 
-            df = df[['Estimate','2.5_ci','97.5_ci','SE','DF','T-stat','P-val']]
+            elif df.shape[1] == 5:
+                #Incase lmerTest chokes it won't return p-values
+                warnings.warn("P-value computation did not occur because lmerTest choked. Check model specification or data...")
+                df.columns =['Estimate','SE','T-stat','2.5_ci','97.5_ci']
+                df = df[['Estimate','2.5_ci','97.5_ci','SE','T-stat']]
 
         elif self.family == 'binomial':
 
@@ -246,18 +284,22 @@ class Lmer(object):
             df.columns = ['Estimate','SE','Z-stat','P-val','2.5_ci','97.5_ci','OR','OR_2.5_ci','OR_97.5_ci','Prob','Prob_2.5_ci','Prob_97.5_ci']
             df = df[['Estimate','2.5_ci','97.5_ci','OR','OR_2.5_ci','OR_97.5_ci','Prob','Prob_2.5_ci','Prob_97.5_ci','SE','Z-stat','P-val']]
 
-        df['Sig'] = df['P-val'].apply(lambda x: self._sig_stars(x))
+        if 'P-Val' in df.columns:
+            df['Sig'] = df['P-val'].apply(lambda x: self._sig_stars(x))
         self.coefs = df
         self.fitted = True
 
         # TODO: Add group names when printing number of groups +enhancement id:1 gh:10
-        print("Number of groups: %s\n" % (self.ngrps))
-        print("Log-likelihood: %.3f \t AIC: %.3f\n" % (self.logLike,self.AIC))
-        print("Random effects:\n")
-        print("%s\n" % (self.ranef_var.round(3)))
-        if self.ranef_corr is not None:
-            print("%s\n" % (self.ranef_corr.round(3)))
+        if summary:
+            print("Number of groups: %s\n" % (self.ngrps))
+            print("Log-likelihood: %.3f \t AIC: %.3f\n" % (self.logLike,self.AIC))
+            print("Random effects:\n")
+            print("%s\n" % (self.ranef_var.round(3)))
+            if self.ranef_corr is not None:
+                print("%s\n" % (self.ranef_corr.round(3)))
+            else:
+                print("No random effect correlations specified\n")
+            print("Fixed effects:\n")
+            return self.coefs.round(3)
         else:
-            print("No random effect correlations specified\n")
-        print("Fixed effects:\n")
-        return self.coefs.round(3)
+            return
