7cea10b305a3f475ae2039142f45f38a13c36c55
ejolly <eshin.jolly@gmail.com>
2017-12-12 15:07:36 -0500

    Fixed setup type.

--- setup.py
+++ setup.py
@@ -20,12 +20,12 @@ setup(
     packages = find_packages(exclude=['pymer4/tests']),
     license = 'LICENSE.txt',
     description='pymer4: all the convenience of lme4 in python',
-    long_description: 'pymer4 is a Python package to make it simple to ',
+    long_description= 'pymer4 is a Python package to make it simple to ',
                       'perform multi-level modeling by interfacing with ',
                       'the popular R package lme4. pymer4 is also capable ',
                       'of fitting a variety of standard regression models ',
-                      'with robust, bootstrapped, and permuted estimators.',
-    keywords = ['statistics','multi-level-modeling','regression','analysis']
+                      'with robust, bootstrapped, and permuted estimators',
+    keywords = ['statistics','multi-level-modeling','regression','analysis'],
     classifers = [
         "Programming Language :: Python",
         "Operating System :: OS Independent",
