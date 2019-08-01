Corrected permutation p-value to return exact precision.

pymer4/stats.py
@@ -194,16 +194,18 @@ def perm_test(
             for i in range(n_perm)
         )
 
+        denom = float(len(perms)) + 1
+
         if tails == 2:
-            p = np.mean(np.abs(perms) >= np.abs(original_stat))
+            numer = np.sum(np.abs(perms) >= np.abs(original_stat)) + 1
         elif tails == 1:
             if original_stat >= 0:
-                p = np.mean(perms >= original_stat)
+                numer = np.sum(perms >= original_stat) + 1
             else:
-                p = np.mean(perms <= original_stat)
+                numer = np.sum(perms <= original_stat) + 1
         else:
             raise ValueError("tails must be 1 or 2")
-
+        p = numer / denom
         if return_dist:
             return original_stat, p, perms
         else:
@@ -370,7 +372,7 @@ def _perm_test(x, y, stat, equal_var, random_state):
             x = x * random_state.choice([1, -1], len(x))
         else:
             shuffled_combined = random_state.permutation(np.hstack([x, y]))
-            x, y = shuffled_combined[: x.size], shuffled_combined[x.size :]
+            x, y = shuffled_combined[:x.size], shuffled_combined[x.size:]
     elif (stat == "tstat-paired") or (y is None):
         x = x * random_state.choice([1, -1], len(x))
 

pymer4/utils.py
@@ -362,9 +362,9 @@ def to_ranks_by_group(dat, group, formula, exclude_cols=[]):
 
 def _perm_find(arr, x):
     """
-    Find permutation cutoff in array.
+    Find permutation cutoff in array. Two-tailed only
     """
-    return np.sum(np.abs(arr) >= np.abs(x)) / float(len(arr))
+    return (np.sum(np.abs(arr) >= np.abs(x)) + 1) / (float(len(arr)) + 1)
 
 
 def isPSD(mat, tol=1e-8):
@@ -380,7 +380,7 @@ def isPSD(mat, tol=1e-8):
 
 def nearestPSD(A, nit=100):
     """
-    Higham (2000) algorithm to find the nearest positive semi-definite matrix that minimizes the Frobenius distance/norm. Sstatsmodels using something very similar in corr_nearest(), but with spectral SGD to search for a local minima. Reference: https://goo.gl/Eut7UU
+    Higham (2000) algorithm to find the nearest positive semi-definite matrix that minimizes the Frobenius distance/norm. Statsmodels using something very similar in corr_nearest(), but with spectral SGD to search for a local minima. Reference: https://goo.gl/Eut7UU
 
     Args:
         nit (int): number of iterations to run algorithm; more iterations improves accuracy but increases computation time.