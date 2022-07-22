d4418afb527be289f9d047496c7206b6bea76bfb
Andrea Manica <am315@cam.ac.uk>
2022-07-22 18:01:18 +0100

    Add tests for lrt
    For the moment, we use a static dump of the R output, as rpy2 seems to clash with pytest

--- pymer4/tests/test_stats.py
+++ pymer4/tests/test_stats.py
@@ -1,6 +1,10 @@
 from __future__ import division
 import numpy as np
-from pymer4.stats import cohens_d, perm_test, boot_func, tost_equivalence, _mean_diff
+import pandas as pd
+import os
+from pymer4.models import Lmer
+from pymer4.utils import get_resource_path
+from pymer4.stats import cohens_d, perm_test, boot_func, tost_equivalence, _mean_diff, lrt
 
 
 def test_cohens_d():
@@ -59,3 +63,73 @@ def test_boot_func():
     result = boot_func(x, y, func=_mean_diff)
     assert len(result) == 2
     assert len(result[1]) == 2
+
+def test_lrt():
+    # read the data and build 3 nexted models
+    df = pd.read_csv(os.path.join(get_resource_path(), "sample_data.csv"))
+    model = Lmer("DV ~ IV3 + IV2 + (1|Group)", data=df)
+    model.fit(summarize=False)
+    model_sub = Lmer("DV ~ IV2 + (1|Group)", data=df)
+    model_sub.fit(summarize=False)
+    model_sub2 = Lmer("DV ~ 1+ (1|Group)", data=df)
+    model_sub2.fit(summarize=False)
+    # lrt test with REML (i.e. WITHOUT refitting the models)
+    lrt_reml = lrt([model, model_sub, model_sub2], refit=False)
+    # the order in which we give the models should not affect the output
+    lrt_reml_scrambled = lrt([model_sub, model, model_sub2], refit=False)
+    assert lrt_reml.equals(lrt_reml_scrambled) is True
+    # now do an lrt with ML (i.e. refittiing the models)
+    lrt_ml = lrt([model, model_sub, model_sub2], refit=True)
+    # refitting with ML does change the output compared to REML
+    assert lrt_reml.equals(lrt_ml) is False
+
+    # pytest crashes with rpy2 in a conda env with python 3.9, so comment out this live test against
+    # R; for the moment we will use the tables generated with this code when running interactively
+
+    # rerun the models directly with rpy2 and compare
+    # import rpy2.robjects as robjects
+    # import rpy2.robjects.packages as rpackages
+    # data = robjects.r('read.table(file ="sample_data.csv", header = T, sep=",")')
+    # lme4 = rpackages.importr('lme4')
+    # rmodel = lme4.lmer("DV ~ IV3 + IV2 + (1|Group)", data=data)
+    # rmodel_sub = lme4.lmer("DV ~ IV2  + (1|Group)", data=data)
+    # rmodel_sub2 = lme4.lmer("DV ~ 1  + (1|Group)", data=data)
+    # r_lrt_reml = lme4.anova_merMod(rmodel, rmodel_sub, rmodel_sub2, refit=False)
+    # r_lrt_ml = lme4.anova_merMod(rmodel, rmodel_sub, rmodel_sub2, refit=True)
+    # # subset the pymer4 and r table to match (remove columns with model names and sign)
+    # lrt_reml_sub = (lrt_reml.drop(lrt_reml.columns[[0, 9]], axis=1))
+    # r_lrt_reml_sub = (pd.DataFrame.from_records(r_lrt_reml)).fillna("")
+    # # make column names identical
+    # r_lrt_reml_sub.columns = lrt_reml_sub.columns
+    # # now assert they are equal
+
+    r_lrt_reml_sub = pd.DataFrame.from_records(
+        np.rec.array([(0, 3., 4832.10121511, 4845.10637787, -2413.05060756, 4826.10121511, '', '', ''),
+                   (1, 4., 4587.9706894, 4605.3109064, -2289.9853447, 4579.9706894, 246.13052571481512, 1.0,
+                    1.8115312634852814e-55),
+                   (2, 5., 4584.81022022, 4606.48549148, -2287.40511011, 4574.81022022, 5.160469174164973, 1.0,
+                    0.02310665453923671)],
+                  dtype=[('index', '<i8'), ('npar', '<f8'), ('AIC', '<f8'), ('BIC', '<f8'), ('log-likelihood', '<f8'),
+                         ('deviance', '<f8'), ('Chisq', 'O'), ('Df', 'O'), ('P-val', 'O')])
+    )
+
+    pd.testing.assert_frame_equal(r_lrt_reml_sub, r_lrt_reml_sub, check_dtype=False)
+    # # and now repeat for the ml table
+    # lrt_ml_sub = (lrt_ml.drop(lrt_ml.columns[[0, 9]], axis=1))
+    # r_lrt_ml_sub = (pd.DataFrame.from_records(r_lrt_ml)).fillna("")
+    # r_lrt_ml_sub.columns = lrt_ml_sub.columns
+
+    r_lrt_ml_sub = pd.DataFrame.from_records(
+        np.rec.array([(0, 3., 4836.69171163, 4849.69687439, -2415.34585582, 4830.69171163, '', '', ''),
+                      (1, 4., 4586.95522083, 4604.29543783, -2289.47761041, 4578.95522083, 251.7364908061154, 1.0,
+                       1.08611087137599e-56),
+                      (2, 5., 4586.10807729, 4607.78334855, -2288.05403865, 4576.10807729, 2.8471435322935577, 1.0,
+                       0.09153644160564649)],
+                     dtype=[('index', '<i8'), ('npar', '<f8'), ('AIC', '<f8'), ('BIC', '<f8'),
+                            ('log-likelihood', '<f8'), ('deviance', '<f8'), ('Chisq', 'O'), ('Df', 'O'),
+                            ('P-val', 'O')])
+    )
+
+
+    pd.testing.assert_frame_equal(r_lrt_ml_sub, r_lrt_ml_sub, check_dtype=False)
+
